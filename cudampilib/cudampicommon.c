e0659918cbefca7b293f7a497192addcad456ca9
Agata Geisler <geisler.agata@wp.pl>
2024-10-27 14:49:07 +0100

    Addec getGpuEnergyUsed function

--- cudampilib/cudampicommon.c
+++ cudampilib/cudampicommon.c
@@ -112,3 +112,29 @@ cudaError_t __cudampi__getCpuFreeThreads(int* count)
 
   return cudaSuccess;
 }
+
+cudaError_t getGpuEnergyUsed(nvmlDevice_t device, float* lastEnergyMeasured, float* energyUsed, float* totalEnergyUsed) {
+    // checks power consumption since last measurement and updates variables.
+
+    nvmlReturn_t result;
+    unsigned int power_mw;
+    float power_watts, energy_joules;
+
+    result = nvmlDeviceGetPowerUsage(device, &power_mw);
+    if (result != NVML_SUCCESS) {
+        fprintf(stderr, "Failed to get power usage: %s\n", nvmlErrorString(result));
+        return cudaErrorUnknown;
+    }
+
+    power_watts = (float)power_mw / 1000.0f;
+
+    energy_joules = power_watts;
+
+    *energyUsed = energy_joules - *lastEnergyMeasured;
+
+    *lastEnergyMeasured = energy_joules;
+
+    *totalEnergyUsed += energy_joules;
+
+    return cudaSuccess;
+}
--- cudampilib/include/cudampicommon.h
+++ cudampilib/include/cudampicommon.h
@@ -21,4 +21,6 @@ float getGPUpower(int gpuid);
 
 cudaError_t getCpuEnergyUsed(float* lastEnergyMeasured, float* energyUsed);
 
+cudaError_t getGpuEnergyUsed(nvmlDevice_t device, float* lastEnergyMeasured, float* energyUsed, float* totalEnergyUsed);
+
 cudaError_t __cudampi__getCpuFreeThreads(int* count);
