3bbb4cdcd27367efe991d1187b25f0ddf2d2a274
Aleksandra Hein <s184717@des14.kask.eti.pg.gda.pl>
2024-11-10 20:52:30 +0100

    draw time(power cap)

--- python_scripts/charts.py
+++ python_scripts/charts.py
@@ -2,13 +2,34 @@ import matplotlib.pyplot as plt
 from models import ExperimentResult
 
 
-def time_powercap_scatter():
-    pass
+def time_powercap_scatter(experiment_result: ExperimentResult):
+    power_cpu_gpu = [multiple_run_result.parameters.powercap for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
+    execution_duration_cpu_gpu_avg = [multiple_run_result.average("execution_duration") for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
+    execution_duration_cpu_gpu_min = [multiple_run_result.min("execution_duration") for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
+    execution_duration_cpu_gpu_max = [multiple_run_result.max("execution_duration") for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
+
+    power_gpu = [multiple_run_result.parameters.powercap for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
+    execution_duration_gpu_avg = [multiple_run_result.average("execution_duration") for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
+    execution_duration_gpu_min = [multiple_run_result.min("execution_duration") for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
+    execution_duration_gpu_max = [multiple_run_result.max("execution_duration") for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
+
+    plt.plot(power_cpu_gpu, execution_duration_cpu_gpu_avg, label="CPU+GPU avg", marker='o')
+    plt.plot(power_gpu, execution_duration_gpu_avg, label="GPU avg", marker='o')
+    plt.plot(power_cpu_gpu, execution_duration_cpu_gpu_min, label="CPU+GPU min", marker='o')
+    plt.plot(power_gpu, execution_duration_gpu_min, label="GPU min", marker='o')
+    plt.plot(power_cpu_gpu, execution_duration_cpu_gpu_max, label="CPU+GPU max", marker='o')
+    plt.plot(power_gpu, execution_duration_gpu_max, label="GPU max", marker='o')
+    plt.xlabel("power cap")
+    plt.ylabel("time [s]")
+    plt.legend()
+    plt.savefig(f'{experiment_result.experiment_result[0].parameters.app_name}_time_power_cap_nodes_{experiment_result.experiment_result[0].parameters.number_od_nodes}.png')
+
 
 def time_batch_size_scatter(experiment_result: ExperimentResult):
     batch_size_cpu_gpu = [multiple_run_result.parameters.batch_size for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
     execution_duration_cpu_gpu = [multiple_run_result.average("execution_duration") for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
-    batch_size_gpu = [multiple_run_result.parameters.batch_size for multiple_run_result in experiment_result.experiment_result if multiple_run_result.parameters.cpu_enabled]
+    
+    batch_size_gpu = [multiple_run_result.parameters.batch_size for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
     execution_duration_gpu = [multiple_run_result.average("execution_duration") for multiple_run_result in experiment_result.experiment_result if not multiple_run_result.parameters.cpu_enabled]
     
     plt.plot(batch_size_cpu_gpu, execution_duration_cpu_gpu, label="CPU+GPU", marker='o')
